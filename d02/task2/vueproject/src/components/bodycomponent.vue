a4456823462803b682d0ddbdc680b64482e0c064
MariamZayed <mariamzayed91@gmail.com>
2023-05-05 00:46:56 +0300

    done task2 d02

--- d02/task2/vueproject/src/components/bodycomponent.vue
+++ d02/task2/vueproject/src/components/bodycomponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <table class="table">
+    <table class="table" v-if="isFormVisible==false">
         <thead>
             <tr>
                 <th>#</th>
@@ -7,12 +7,12 @@
                 <th>city</th>
             </tr>
         </thead>
-        <tbody>
-            <tr v-for="student in students" :key="student.id">
-                <td>{{ student.id }}</td>
-                <td>{{ student.name }}</td>
-                <td>{{ student.city }}</td>
-            </tr>
+        <tbody >
+                <tr v-for="student in students" :key="student.id">
+                    <td>{{ student.id }}</td>
+                    <td>{{ student.name }}</td>
+                    <td>{{ student.city }}</td>
+                </tr>                
         </tbody>
         <tfoot>
             <tr>
@@ -20,8 +20,33 @@
                     <h5 class="alert alert-info col-12 text-center"> {{msg}} {{ students.length }}</h5>
                 </td>
             </tr>
+            <tr>
+                <td colspan="3">
+                    <button  @click.prevent="isFormVisible=true" class=" btn btn-primary">
+                    Add to table
+                    </button>
+                </td>
+            </tr>
         </tfoot>
     </table>
+    <div v-if="isFormVisible==true">
+        <button @click.prevent="isFormVisible=false" class="btn btn-primary" > Show Student Table</button>    
+        <form method="get" action="#" >
+            <div class="mb-3">
+                <label class="form-label">id</label>
+                <input type="text" class="form-control" v-model="student.id" id="id">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Name</label>
+                <input type="text" class="form-control" v-model="student.name" id="name">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">city</label>
+                <input type="text" class="form-control" v-model="student.city" id="city">
+            </div>
+            <button type="submit" class="btn btn-primary" @click.prevent=addToStudentTable(student)>Submit</button>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -30,8 +55,20 @@ export default {
     data(){
         return{
             msg:'total num of students = ',
-            students
+            students,
+            isFormVisible:false,
+            student:{
+                id:'',
+                name:"",
+                city:''
+            }
         }
-    }
+    },
+    methods:{
+        addToStudentTable(student){
+            this.students.push(student);
+            console.log(this.students.length);
+        },
+    },
 }
 </script>
--- d02/task2/vueproject/src/components/footercomponent.vue
+++ d02/task2/vueproject/src/components/footercomponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
-        <h3 class="col-12 alert bg-light p-5 text-center">
+        <h5 class="col-12 alert bg-light p-5 text-center">
             copy rights &copy; reserved to open source Mansoura
-        </h3>
+        </h5>
     </div>
-</template>
+</template> 
